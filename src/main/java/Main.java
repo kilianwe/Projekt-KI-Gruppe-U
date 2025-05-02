1bbd255fa061334c1af2d3db7e3de05c1dd99361
ep <ep@trustami.com>
2025-05-02 18:01:51 +0200

    faster debugging

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Piece[][] board = BoardInitializer.createStartingPosition();
-        System.out.println(BoardInitializer.toString(board));
-
-        // Generate and print all moves for white
-        List<Move> whiteMoves = GuardTowersMoveGenerator.generateMoves(board, true);
-        for (Move move : whiteMoves) {
-            System.out.println(move + " -> " + move.toAlgebraic());
-        }
-        System.out.println(whiteMoves.size() + " moves generated.");
-
         Board bitboard0 = new Board();
         BitBoardUtils utils = new BitBoardUtils();
         List<BitBoardUtils.MovePair> movePairs = utils.generateAllLegalMoves("R", bitboard0);
